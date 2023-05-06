4ee9c1fe84575d0500138b6286fd205ceb910a92
pansan98 <edo.fjsjm@icloud.com>
2023-05-06 17:03:34 +0900

    セクション3-1：interface

---
+++ study/five/src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+fun main(args: Array<String>) {
+    val adult = Person("Adult", "adult@example.com")
+    adult.say()
+    val child = Student("Child", "child@example.com", 3)
+    child.say()
+    val animal = Animals("ゴリラ", "4足歩行")
+    animal.say()
+    val arr:Array<Vector> = arrayOf<Vector>(adult, child, animal)
+    println(arr)
+
+    val v = Vec {println("Hello," + it + "!")}
+    v.say("Vec")
+
+    val fni = fninterface {
+        name:String, age:Int ->
+            println("Hello," + name + "(" + age + ")さん")
+    }
+    fni.say("fnInterface", 5)
+}
+
+interface Vector {
+    var name:String
+    fun say()
+}
+
+open class Person(override var name: String = "noname", var email: String = ""):Vector {
+    override fun say() {
+        println("Name:" + this.name + ", Email:" + this.email)
+    }
+}
+
+class Student(name:String = "noname", email:String = "", var grade:Int = 0):Person(name, email) {
+    override fun say() {
+        println("Name:" + this.name + ", Email:" + this.email + ", Grade:" + this.grade)
+    }
+}
+
+class Animals(override var name:String = "animals", var type:String = ""):Vector {
+    override fun say() {
+        println("Name:" + this.name + ", 種別:" + this.type)
+    }
+}
+
+fun interface Vec {
+    fun say(name:String)
+}
+
+fun interface fninterface {
+    fun say(name:String, age:Int)
+}
