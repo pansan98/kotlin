bc375803fe1bf49b73da9547e075bb13659ba60d
pansan98 <edo.fjsjm@icloud.com>
2023-05-20 16:08:17 +0900

    セクション4-3：thread join

--- study/thirteen/src/main/kotlin/Main.kt
+++ study/thirteen/src/main/kotlin/Main.kt
@@ -2,12 +2,11 @@ import java.util.*
 import kotlin.concurrent.timer
 import kotlin.concurrent.thread
 
-// TODO threadのjoinから
-
 fun main(args: Array<String>) {
     //fntimer()
     //fnthread()
-    fnmultithread()
+    //fnmultithread()
+    fnthreadjoin()
 }
 
 fun fntimer() {
@@ -62,4 +61,46 @@ fun fnmultithread() {
 
     t2.start()
     t1.start()
+}
+
+fun fnthreadjoin() {
+    val count = 10
+    var t1:Thread
+    var t2:Thread
+    var t3:Thread
+
+    t1 = thread {
+        for(n in 1..count) {
+            println("* thread1(${n}) *")
+            Thread.sleep(10L)
+        }
+        println("thread1 finished")
+    }
+
+    t2 = thread {
+        for(n in 1..count) {
+            println("* thread2(${n}) *")
+            Thread.sleep(10L)
+            runCatching {
+                when(n % 2) {
+                    0 -> t1.join(10L)
+                }
+            }
+        }
+        println("thread2 finished")
+    }
+
+    t3 = thread {
+        for(n in 1..count) {
+            println("* thread3(${n}) *")
+            Thread.sleep(10L)
+            runCatching {
+                when(n % 3) {
+                    0 -> t1.join(10L)
+                    1 -> t2.join(10L)
+                }
+            }
+        }
+        println("thread3 finished")
+    }
 }
