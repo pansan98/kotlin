213f6b02e8325177a31457cf642414a48b233069
pansan98 <edo.fjsjm@icloud.com>
2023-05-27 16:17:02 +0900

    コンポーネントの追加と操作。 Fragmentは今の参考書では古いのでMainActivityで再現

--- study/androidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ study/androidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.androidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidapp.databinding.ActivityMainBinding
+import android.widget.Toast
+import android.widget.*
+import android.widget.SeekBar.OnSeekBarChangeListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,9 +19,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            val txt = binding.editText.text
-            binding.message.text = "Hello, ${txt}"
+        binding.switch1.setOnCheckedChangeListener {btn: CompoundButton, f: Boolean ->
+            Toast.makeText(binding.root.context, "checked:${f}.", Toast.LENGTH_SHORT).show()
         }
+
+        binding.radioGroup.setOnCheckedChangeListener {group: RadioGroup, id: Int ->
+            val ob = findViewById<RadioGroup>(id)
+            //Toast.makeText(binding.root.context, "checked: ${id}", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object: OnSeekBarChangeListener {
+            var vol:Int = 0
+            override fun onProgressChanged(sb: SeekBar, value: Int, f: Boolean) {
+                vol = value
+            }
+
+            override fun onStartTrackingTouch(sb: SeekBar) {}
+
+            override fun onStopTrackingTouch(sb: SeekBar) {
+                Toast.makeText(binding.root.context, "値：${vol}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+//        binding.button.setOnClickListener {
+//            val txt = binding.editText.text
+//            binding.message.text = "Hello, ${txt}"
+//            Toast.makeText(binding.root.context, "こんにちは、${txt}さん", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
