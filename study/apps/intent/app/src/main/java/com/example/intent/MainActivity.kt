dc3612253b7306681446a0e66b4bfe46d9a585c4
pansan98 <edo.fjsjm@icloud.com>
2023-10-15 15:55:16 +0900

    リストViewのカスタマイズ

--- study/apps/intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ study/apps/intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -7,50 +7,60 @@ import android.widget.*
 import android.view.*
 
 class MainActivity : AppCompatActivity() {
+    // Viewに表示するリストデータ
+    private var _menulist: MutableList<MutableMap<String, Any>> = mutableListOf()
+    // SimpleAdaptor の第4引数
+    private val _from = arrayOf("name", "price")
+    // SimpleAdaptor の第5引数
+    private val _to = intArrayOf(R.id.tvMenuNameRow, R.id.tvMenuPriceRow)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // 画面部品のListViewを取得
         val lvMenu = findViewById<ListView>(R.id.lvMenu)
-        // SampleAdapterで仕様するMutableListオブジェを用意
-        val menulist: MutableList<MutableMap<String, String>> = mutableListOf()
-        // メニューデータの登録
-        var menu = mutableMapOf("name" to "からあげ定食", "price" to "800円")
-        menulist.add(menu)
-        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "900円")
-        menulist.add(menu)
-        menu = mutableMapOf("name" to "味噌さば定食", "price" to "750円")
-        menulist.add(menu)
-        menu = mutableMapOf("name" to "アジフライ定食", "price" to "850円")
-        menulist.add(menu)
-        menu = mutableMapOf("name" to "牛丼", "price" to "560円")
-        menulist.add(menu)
-        menu = mutableMapOf("name" to "卵かけご飯", "price" to "180円")
-        menulist.add(menu)
 
-        // SimpleAdapter第4引数fromデータの用意
-        val from = arrayOf("name", "price")
-        // SimpleAdapter第5引数toデータの用意
-        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        _menulist = createTeishokuList()
+
         // SimpleAdapterの用意
-        val adapter = SimpleAdapter(this@MainActivity, menulist, android.R.layout.simple_list_item_2, from, to)
+        val adapter = SimpleAdapter(this@MainActivity, _menulist, R.layout.row, _from, _to)
         lvMenu.adapter = adapter
         lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
+        // 定食メニューリストのListオブジェクトを用意
+        val menulist: MutableList<MutableMap<String, Any>> = mutableListOf()
+        // 定食データを格納するMapオブジェクトの用意とmenulistへ追加
+        var menu = mutableMapOf<String, Any>("name" to "からあげ定食", "price" to 800, "desc" to "若鶏のからあげにサラダ、ご飯とお味噌汁が付きます。")
+        menulist.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+        menulist.add(menu)
+        menu = mutableMapOf("name" to "味噌さば定食", "price" to 750, "desc" to "サラダとご飯とお味噌汁が付きます。")
+        menulist.add(menu)
+        menu = mutableMapOf("name" to "アジフライ定食", "price" to 850, "desc" to "ご飯だけ付きます。")
+        menulist.add(menu)
+        menu = mutableMapOf("name" to "牛丼", "price" to 560, "desc" to "ご飯の大盛りや頭大盛りが追加料金で選択できます。")
+        menulist.add(menu)
+        menu = mutableMapOf("name" to "卵かけご飯", "price" to 180, "desc" to "醤油は自由にお使い頂けます。おかわりは無料です。")
+        menulist.add(menu)
+
+        return menulist
     }
 
     private inner class ListItemClickListener : AdapterView.OnItemClickListener {
         override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
             // タップされた行のデータを取得。SimpleAdapterでは1行のデータはMutableMap型になる
-            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+            val item = parent.getItemAtPosition(position) as MutableMap<String, Any>
             // 定食名と金額
-            val menuName = item["name"]
-            val menuPrice = item["price"]
+            val menuName = item["name"] as String
+            val menuPrice = item["price"] as Int
             // インテントオブジェクトを生成
             val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
             // 第2画面に送るデータを格納
             intent2MenuThanks.putExtra("name", menuName)
-            intent2MenuThanks.putExtra("price", menuPrice)
+            intent2MenuThanks.putExtra("price", "${menuPrice}円")
             // 第2画面の起動
             startActivity(intent2MenuThanks)
         }
